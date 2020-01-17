ee6ecfe4a93108ae94c75b6fdd65a862c31b5773
Alex Miller <alexmillerprojects@gmail.com>
2020-01-17 10:53:10 -0800

    finished sprint

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,44 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function tabMaker(obj){
+    const newTopic = document.createElement('div')
+
+    newTopic.classList.add('tab')
+
+    newTopic.textContent = obj
+}
+
+const tabs = document.querySelector('.topics')
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics').then(res =>{
+    console.log(res)
+    const newArray = Object.values(res.data.topics)
+    console.log(newArray)
+    newArray.forEach(item =>{
+        tabs.append(tabMaker(newArray))
+    })
+
+ })
+
+     
+
+
+
+
+
+
+
+
+
+
+ //     const topicTabs = res.data.topics
+//    res.data.topics.forEach(data =>{
+//         const newTopic = document.createElement('div')
+//         newTopic.classList.add('tab')
+//         newTopic.textContent = res.data.topics[data]
+//         tabs.append(newTopic)
+//      //    console.log(newTopic.textContent)
+//      console.log(res.data.topics[data])
+//     })
